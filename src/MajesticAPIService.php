fcfca09b86e6630140825a4fb6ecea7c5aff028c
Nenad Tičarić <nticaric@gmail.com>
2014-10-09 14:25:55 +0200

    execute command

--- src/MajesticAPIService.php
+++ src/MajesticAPIService.php
@@ -3,9 +3,27 @@
 use GuzzleHttp\Client;
 
 class MajesticAPIService {
+
+    private $endpoint = "http://api.majestic.com/api/";
     
     public function __construct($apiKey, $sandbox = false)
     {
-        
+        if($sandbox == true) {
+            $this->endpoint = "http://developer.majestic.com/api";
+        }
+        $this->responseType = "json";
+        $this->apiKey = $apiKey;
+    }
+
+    public function executeCommand($command, $params)
+    {
+        $client = new Client;
+
+        $params["cmd"]         = $command;
+        $params["app_api_key"] = $this->apiKey;
+
+        return $client->get($this->endpoint ."/". $this->responseType, [
+            'query' => $params
+        ]);
     }
 }
